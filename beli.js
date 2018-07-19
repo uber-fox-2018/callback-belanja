6e54babf9666c56810e0087e5e57ee70c82f0dd4
Khodhi Robbani <khodhirobbani@gmail.com>
2018-07-19 15:45:00 +0700

    toko khodhi

--- beli.js
+++ beli.js
@@ -3,13 +3,20 @@ function beli(uang, obj, cb){
   setTimeout(function(){
     let kembalian = uang - obj.harga
     if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
+      console.log(`Saya sudah membeli ${obj.item} kurangnya ${kembalian}`);
       cb(kembalian)
     }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
+      console.log(`uang gk cukup nih buat beli ${obj.item} kurang uang ${Math.abs(kembalian)}`);
       cb(0)
     }
   }, obj.waktu);
 }
 
 module.exports = beli;
+
+
+//uang => jumlah uang yang dbawa
+// objItm => {item: 'rokok',harga: 2000,waktu:5}
+/* callback => function kembalian (kembalian){
+
+} */
